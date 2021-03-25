46e412d6d52412ce236b36390f17b88f617d5586
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-03-25 01:55:04 -0600

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
---This file contains the DB schema that creates the DB
+/*This file contains the DB schema that creates the DB*/
 
 DROP DATABASE IF EXISTS employee_mgmtDB;
 
@@ -7,24 +7,24 @@ CREATE database employee_mgmtDB;
 USE employee_mgmtDB;
 
 CREATE TABLE employees (
-   emp_id INTEGER(5) AUTO_INCREMENT NOT NULL,
+   emp_id INTEGER AUTO_INCREMENT NOT NULL,
    first_name VARCHAR(30) NOT NULL,
    last_name VARCHAR(30) NOT NULL,
-   role_id INTEGER(2) NOT NULL,
-   manager_id INTEGER(2),
+   role_id INTEGER NOT NULL,
+   manager_id INTEGER, /* NULL is OK because employees may not have a manager*/
    PRIMARY KEY (emp_id)
 );
 
-CREATE TABLE emp_role (
-   role_id  INTEGER(2) NOT NULL,
+CREATE TABLE emp_roles (
+   role_id INTEGER AUTO_INCREMENT NOT NULL,
    title VARCHAR(50) NOT NULL,
-   salary INTEGER(6) NOT NULL,
-   department_id INTEGER(2) NOT NULL,
+   salary INTEGER NOT NULL,
+   department_id INTEGER NOT NULL,
    PRIMARY KEY (role_id)
 );
 
-CREATE TABLE department (
-   dept_id INTEGER(2) NOT NULL,
+CREATE TABLE departments (
+   dept_id INTEGER AUTO_INCREMENT NOT NULL,
    dept_name VARCHAR(50) NOT NULL,
    PRIMARY KEY (dept_id)
 );
